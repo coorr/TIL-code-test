279fbd95732ff3184d716d2e1ce781324fe02098
김진성 <wlsdiqkdrk@naver.com>
2023-08-20 13:26:44 +0900

    재세팅

---
+++ src/main/java/com/example/java/Main.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
