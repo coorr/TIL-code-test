513ed7d619015734748b9658eb820c2906afde89
김진성 <wlsdiqkdrk@naver.com>
2023-11-16 15:04:53 +0900

    송아지 찾기(BFS)

---
+++ src/main/java/com/example/java/november/송아지찾기.java
@@ -0,0 +1,62 @@
+package com.example.java.november;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ title. 송아지 찾기 1(BFS : 상태트리탐색)
+
+ 설명
+ 현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다.
+ 현수의 위치와 송아지의 위치가 수직선상의 좌표 점으로 주어지면 현수는 현재 위치에서 송아지의 위치까지 다음과 같은 방법으로 이동한다.
+ 송아지는 움직이지 않고 제자리에 있다.
+ 현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다.
+ 최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성하세요.
+
+ 입력
+ 첫 번째 줄에 현수의 위치 S와 송아지의 위치 E가 주어진다. 직선의 좌표 점은 1부터 10,000까지이다.
+
+ 출력
+ 점프의 최소횟수를 구한다. 답은 1이상이며 반드시 존재합니다.
+
+ 예시 입력 1
+ 5 14
+
+ 예시 출력 1
+ 3
+ */
+public class 송아지찾기 {
+    public void solution() {
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        int e = scanner.nextInt();
+
+        System.out.println(getLevel(e, s));
+    }
+
+    private int getLevel(int e, int s) {
+        Queue<Integer> queue = new LinkedList<>();
+        int[] dis = {1, -1, 5};
+        int[] check = new int[10001];
+        check[s] = 1;
+        queue.offer(s);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Integer n = queue.poll();
+                for (int di : dis) {
+                    int nx = n + di;
+                    if (nx == e) return level + 1;
+                    if (nx >= 1 && nx <= 10000 && check[nx] == 0) {
+                        check[nx] = 1;
+                        queue.offer(nx);
+                    }
+                }
+            }
+            level++;
+        }
+        return 1;
+    }
+}
